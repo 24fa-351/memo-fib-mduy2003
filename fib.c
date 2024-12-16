de70abe8798611fac24bd7583c004c645aff4547
mduy2003 <mduy2003@csu.fullerton.edu>
2024-12-16 00:51:32 -0800

    Reformatted

--- fib.c
+++ fib.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 unsigned long long int fibCache[] = {};
 
@@ -8,63 +8,56 @@ unsigned long long int fibIterative(int fibValue);
 
 unsigned long long int fibRecursiveProvider(int fibValue)
 {
-   if (fibValue == 0 || fibValue == 1)
-   {
-      return fibValue;
-   }
-   return fibRecursive(fibValue - 1) + fibRecursive(fibValue - 2);
+    if (fibValue == 0 || fibValue == 1) {
+        return fibValue;
+    }
+    return fibRecursive(fibValue - 1) + fibRecursive(fibValue - 2);
 }
 
 unsigned long long int fibIterativeProvider(int fibValue)
 {
-   unsigned long long int fibSum = 0;
-   unsigned long long int prevNum = 0;
-   unsigned long long int currentNum = 1;
-   for (size_t ix = 1; ix < fibValue; ++ix)
-   {
-      fibSum = prevNum + currentNum;
-      prevNum = currentNum;
-      currentNum = fibSum;
-   }
-   return fibSum;
+    unsigned long long int fibSum = 0;
+    unsigned long long int prevNum = 0;
+    unsigned long long int currentNum = 1;
+    for (size_t ix = 1; ix < fibValue; ++ix) {
+        fibSum = prevNum + currentNum;
+        prevNum = currentNum;
+        currentNum = fibSum;
+    }
+    return fibSum;
 }
 
 unsigned long long int fibRecursive(int fibValue)
 {
-   if (fibCache[fibValue] == 0)
-   {
-      fibCache[fibValue] = fibRecursiveProvider(fibValue);
-   }
-   return fibCache[fibValue];
+    if (fibCache[fibValue] == 0) {
+        fibCache[fibValue] = fibRecursiveProvider(fibValue);
+    }
+    return fibCache[fibValue];
 }
 
 unsigned long long int fibIterative(int fibValue)
 {
-   if (fibCache[fibValue] == 0)
-   {
-      fibCache[fibValue] = fibIterativeProvider(fibValue);
-   }
-   return fibCache[fibValue];
+    if (fibCache[fibValue] == 0) {
+        fibCache[fibValue] = fibIterativeProvider(fibValue);
+    }
+    return fibCache[fibValue];
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-   int firstNum;
-   int input = sscanf(argv[1], "%u", &firstNum);
-   char fibType = *argv[2];
+    int firstNum;
+    int input = sscanf(argv[1], "%u", &firstNum);
+    char fibType = *argv[2];
 
-   int nthNum = firstNum - 1;
+    int nthNum = firstNum - 1;
 
-   if (fibType == 'r')
-   {
-      unsigned long long int result = fibRecursive(nthNum);
-      printf("%llu", result);
-   }
-   else if (fibType == 'i')
-   {
-      unsigned long long int result = fibIterative(nthNum);
-      printf("%llu", result);
-   }
+    if (fibType == 'r') {
+        unsigned long long int result = fibRecursive(nthNum);
+        printf("%llu", result);
+    } else if (fibType == 'i') {
+        unsigned long long int result = fibIterative(nthNum);
+        printf("%llu", result);
+    }
 
-   return 0;
+    return 0;
 }
